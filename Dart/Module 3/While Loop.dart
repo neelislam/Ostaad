a7280a6a3d224a12c674dbb071a33405df6b026c
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-03-17 01:13:09 +0600

    v3

--- Dart/Module 3/While Loop.dart
+++ Dart/Module 3/While Loop.dart
@@ -1,25 +1,25 @@
 main(){
-  //int i  = 1;
-  //
-  // while (i<=10){
-  //   print('Hello world');
-  // i++;
-  // }
-  // while (i<=10){
-  //   print("15 x $i = ${15*(i)}");
-  //   i++;
-  // }
-  // List <String> student = ['Rahim', 'Toufik', 'dojo', 'riff'];
-  //
-  // List <int> due = [100,200,400,500];
-  // int index = 0;
-  // while (index< student.length){
-  //   print("${student[index]} Your due is ${due[index]}");
-  //   index++;
-  // }
-  int i =0;
-  do{
-    print('Hello $i times');
+  int i  = 1;
+
+  while (i<=10){
+    print('Hello world');
+  i++;
+  }
+  while (i<=10){
+    print("15 x $i = ${15*(i)}");
     i++;
-  }while(i<=11);
+  }
+  List <String> student = ['Rahim', 'Toufik', 'dojo', 'riff'];
+
+  List <int> due = [100,200,400,500];
+  int index = 0;
+  while (index< student.length){
+    print("${student[index]} Your due is ${due[index]}");
+    index++;
+  }
+  int j =0;
+  do{
+    print('Hello $j times');
+    j++;
+  }while(j<=11);
 }
