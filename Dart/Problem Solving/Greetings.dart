a289e75d191e3643adb41ce2d43db5f94ef864ca
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-03-28 16:05:38 +0600

    Celsius to fahrenheit

---
+++ Dart/Problem Solving/Greetings.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+main(){
+  String ? S  = stdin.readLineSync()!;
+  print('Hello, $S! Nice to meet you.');
+
+}
---
+++ Dart/Problem Solving/Minimal Yet Important.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+main() {
+  String? input = stdin.readLineSync();
+  if (input != null) {
+    List<String> numbers = input.split(' ');
+    if (numbers.length == 3) {
+      int? number1 = int.tryParse(numbers[0]);
+      int? number2 = int.tryParse(numbers[1]);
+      int? number3 = int.tryParse(numbers[2]);
+
+      int? smallest = number1;
+      if (number2! < smallest!) {
+        smallest = number2;
+      }
+      else if (number3! < smallest) {
+        smallest = number3;
+      }
+      print(smallest);
+    }
+  } else {
+    print("Invalid Input! Try agai...!");
+  }
+}
