591b00ea35c535797185c1cba882009d23ef4183
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-03-30 02:09:02 +0600

    Rectangle measurement

--- Dart/Problem Solving/Rectangle Measurement.dart
+++ Dart/Problem Solving/Rectangle Measurement.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 main(){
-
-
+ String? input = stdin.readLineSync();
+ var numbers = input?.split(' ');
+int? len = int.tryParse(numbers![0]);
+int? wid = int.tryParse(numbers[1]);
+print(len!*wid!);
 }
