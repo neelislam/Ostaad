521ddc0219a972aebc7269ee037b176a7c83799d
neel_islam <rabiul17.ri@gmail.com>
2025-03-29 19:43:05 +0600

    Triangle Area Find but in Int

---
+++ Dart/Problem Solving/Triangle Area finding.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+main(){
+  String? input = stdin.readLineSync();
+  var numbers = input?.split(' ');
+  int? len = int.tryParse(numbers![0]);
+  int? wid = int.tryParse(numbers[1]);
+
+ print(AreaFinding(len!, wid!));
+}
+AreaFinding(int len, int wid) => (0.5*(len*wid)).toInt();
