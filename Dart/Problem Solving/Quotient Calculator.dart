4836978370636d8ccd5dd29f7f76cd9a26749926
Rabiul Islam Apu (Neel) <rabiul17.ri@gmail.com>
2025-03-31 10:34:02 +0600

    Remainder Reminder

---
+++ Dart/Problem Solving/Quotient Calculator.dart
@@ -0,0 +1,8 @@
+import 'dart:io';
+main(){
+  String? input = stdin.readLineSync()!;
+  var numbers = input.split(' ');
+  int? num1 = int.tryParse(numbers[0]);
+  int? num2 = int.tryParse(numbers[1]);
+  print((num1!/num2!).toInt());
+}
